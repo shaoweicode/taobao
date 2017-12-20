bdb6b17e89181b7aa6854795af25951ded8a1448
shaoweicode <417096554@qq.com>
2017-12-21 00:01:29 +0800

    done

--- meishi.py
+++ meishi.py
@@ -9,12 +9,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common.exceptions import TimeoutException
 browser = webdriver.Firefox()
 
+wait = WebDriverWait(browser,10)
+
+
 def search():
-    browser.get('https://www.taobao.com')
-    input = WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, '#q'))
-    )
-    submit = 
+    try:
+        browser.get('https://www.taobao.com')
+        input = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '#q'))
+        )
+        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#J_TSearchForm > div.search-button > button')))
+        input.send_keys('美食')
+        submit.click()
+        total = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'.total')))
+        return total.text                                            
+    except TimeoutException:
+        return search()
+def main():
+    total = search()
+    print(total)
+    
+if __name__=='__main__':
+    main()
